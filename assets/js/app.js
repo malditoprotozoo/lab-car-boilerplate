149906572756e3d5706ed44f62b373d984671c64
malditoprotozoo <virodriguez.fe@gmail.com>
2017-12-04 12:53:10 -0300

    Functions added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,31 @@
+/* Función que determina el tamaño del margen superior
+de header */
+
+var navbarHeight = $(".navbar").height();
+
+$(window).on("load", function() {
+  $("#header").css({ marginTop : navbarHeight + 64 + "px" });
+});
+
 $(window).scroll(function() {
   if ($(document).scrollTop() > 50) {
-    $("#navbar").addClass('shrink');
-    $("#navbar").removeClass("half-basis-height");
+    $("#header").css({ marginTop : navbarHeight + "px" });
+  } else {
+    $("#header").css({ marginTop : navbarHeight + 64 + "px" });
+  }
+});
+
+/* Función que permite que la barra de navegación se agrande
+o encoja según el scroll */
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 50) {
+    $("#navbar").removeClass("padding-both-small");
+    $("#navbar").addClass("shrink");
     $("#logo1").addClass("invisible");
     $("#logo2").removeClass("invisible");
   } else {
+    $("#navbar").addClass("padding-both-small");
     $("#navbar").removeClass("shrink");
-    $("#navbar").addClass("half-basis-height");
     $("#logo2").addClass("invisible");
     $("#logo1").removeClass("invisible");
   }
